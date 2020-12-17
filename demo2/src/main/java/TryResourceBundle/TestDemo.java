67f896ae4cc090b0d0efb9dc26d65ccbde7c3aa7
hobossa <hoboss@gmail.com>
2020-12-17 23:38:37 +0800

    problem MissingResourceException

--- demo2/src/main/java/TryResourceBundle/TestDemo.java
+++ demo2/src/main/java/TryResourceBundle/TestDemo.java
@@ -5,6 +5,8 @@ import java.util.ResourceBundle;
 
 public class TestDemo {
     public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println(loc);
         Locale enUS = new Locale("en", "US");
         ResourceBundle rb = ResourceBundle.getBundle("TryResourceBundle.Messages", enUS);
         System.out.println(rb.getString("hello"));
