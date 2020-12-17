7e252890ce69c41ba4c078013ec4c05912a2069a
hobossa <hoboss@gmail.com>
2020-12-18 00:08:21 +0800

    none

---
+++ demo2/src/main/java/TryIO/TryIO1.java
@@ -0,0 +1,28 @@
+package TryIO;
+
+import java.beans.JavaBean;
+import java.io.File;
+import java.io.IOException;
+
+public class TryIO1 {
+    public static void main(String[] args) throws IOException {
+
+        String current = new File(".").getCanonicalPath();
+        System.out.println(current);
+
+        File file = new File("/Users/");
+        if (file.isDirectory()) {
+            File[] results = file.listFiles();
+            for (File f :
+                    results) {
+                System.out.println(f);
+            }
+        }
+        System.out.println(file.getPath());
+        if (file.exists()){
+            file.delete();
+        } else {
+            System.out.println("create a new file.");
+        }
+    }
+}
---
+++ demo2/src/main/java/TryIO/TryIO2.java
@@ -0,0 +1,7 @@
+package TryIO;
+
+public class TryIO2 {
+    public static void main(String[] args) {
+        
+    }
+}
